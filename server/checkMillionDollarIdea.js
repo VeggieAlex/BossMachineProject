a30aa2c7efbb6a65efbe30d96231976d10663abf
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2021-08-10 16:29:39 +0100

    First commit

---
+++ server/checkMillionDollarIdea.js
@@ -0,0 +1,13 @@
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue }= req.body;
+    if(!numWeeks || !weekleyRevenue)
+    return res.status(400).send("numWeeks and weeklyRevenue properties are required");
+
+    const total = Number(numWeeks) * Number(weeklyRevenue);
+    if(is NaN(total) || total < 1000000)
+    return res.status(400).send("Idea must at least be worth 1M");
+    next();
+};
+
+// Leave this exports assignment so that the function can be used elsewhere
+module.exports = checkMillionDollarIdea;
